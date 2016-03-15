9f959fb512e2074180ba06b02133a079fe225045
CoderAFI <developer_afi@163.com>
2016-03-15 11:44:43 +0800

    修改工程配置编译通过。

--- Package.swift
+++ Package.swift
@@ -3,14 +3,13 @@ import PackageDescription
 let package = Package (
     name: "SwiftGGAppServer",
     dependencies: [
-        .Package(url: "https://github.com/qutheory/vapor.git", majorVersion: 0),
-        .Package(url: "https://github.com/CoderAFI/Stencil.git", majorVersion: 0)
+        .Package(url: "https://github.com/qutheory/vapor.git", majorVersion: 0)
     ],
     exclude: [
-      "Deploy",
-      "Public",
-      "Resources",
-		  "Tests",
-      "Database"
+        "Deploy",
+        "Public",
+        "Resources",
+		"Tests",
+		"Database"
     ]
 )
---
+++ Sources/main.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  SwiftGGAppServer
+//
+//  Created by 孙燕飞 on 16/3/15.
+//  Copyright © 2016年 SwiftGG. All rights reserved.
+//
+
+import Vapor
+
+let app = Application()
+
+app.get("/") { request in
+    return "Welcome to Vapor"
+}
+
+app.start(port: 8080)
+
