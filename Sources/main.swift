8d0135337a21670135f26964814a6a1d1d7d7462
CoderAFI <developer_afi@163.com>
2016-03-15 12:31:35 +0800

    删除注释代码。

--- Sources/main.swift
+++ Sources/main.swift
@@ -10,14 +10,6 @@ import Vapor
 
 let app = Application()
 
-// V1.0接口规范
-//array("/v1/user/userLogin" , "User:userLoginV1"),
-//array("/v1/user/getInfo" , "User:getInfoV1"),
-//array("/v1/article/getCategoryList" , "Article:getCategoryListV1"),
-//array("/v1/article/getArticlesByCategory" , "Article:getArticlesByCategoryV1"),
-//array("/v1/article/getDetail" , "Article:getDetailV1"),
-//array("/v1/catch/newArticle" , "Catch:addNewArticle"),
-
 app.get("/") { request in
     return "Hello SwiftGG!"
 }
@@ -33,8 +25,6 @@ app.post("/v1/article/getCategoryList", handler: ArticleController().getCategory
 app.post("/v1/article/getDetail", handler: ArticleController().getDetailV1)
 
 app.post("/v1/catch/newArticle", handler: CatchController().addNewArticle)
-
-
 
 
 app.start(port: 4000)
