b761814d8e4ce2976a0c0ccf0f110f67d591bdd7
CoderAFI <developer_afi@163.com>
2016-03-17 19:35:53 +0800

    add CryptoSwift support.

--- Package.swift
+++ Package.swift
@@ -4,7 +4,8 @@ let package = Package (
     name: "SwiftGGAppServer",
     dependencies: [
         .Package(url: "https://github.com/qutheory/vapor.git", majorVersion: 0),
-        .Package(url: "https://github.com/novi/mysql-swift.git", majorVersion: 0)
+        .Package(url: "https://github.com/novi/mysql-swift.git", majorVersion: 0),
+        .Package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", majorVersion: 0)
     ],
     exclude: [
         "Deploy",
--- Sources/SwiftGGAppServer/Controllers/UserController.swift
+++ Sources/SwiftGGAppServer/Controllers/UserController.swift
@@ -8,7 +8,7 @@
 
 import Vapor
 import MySQL
-//import Foundation
+import CryptoSwift
 
 class UserController : Controller {
 
@@ -24,35 +24,51 @@ class UserController : Controller {
         let options = Options(host: "123.57.250.194", port: 3306, user: "swiftggapp", password: "SwiftGG123", database: "swiftggapp")
         return ConnectionPool(options: options)
     }()
-    
+
     required init() {
-        
+
     }
-    
+
     func loginV1(request: Request) throws -> ResponseConvertible {
-        return ""
+
+        let params = request.parameters
+        let username = params["userName"]
+        let password = params["password"]
+
+        // 参数判空
+        // 判断是否含有特殊字符
+        // 判断长度
+        // 查询账号信息
+
+        let queryParams: (String, String) = ( username!, password!.md5() )
+
+        let (rows, _): ([User], QueryStatus) = try pool.execute { conn in
+            try conn.query("SELECT id,account,password,nickname FROM sg_user where account = ? and password = ?", build(queryParams)) as ([User], QueryStatus)
+        }
+
+        return try Json(["userId": rows[0].id])
     }
 
     func otherLoginV1(request: Request) throws -> ResponseConvertible {
         return ""
     }
-    
+
     func registerV1(request: Request) throws -> ResponseConvertible {
         return try Json(["lub": "dub"])
     }
-    
+
     func getInfoV1(request: Request) throws -> ResponseConvertible {
-        
+
         let (rows, _): ([User], QueryStatus) = try pool.execute { conn in
             try conn.query("SELECT id,account,password,nickname FROM sg_user;") as ([User], QueryStatus)
         }
-        
+
         print(rows)
-        
+
         let result = try rows.map { try Json(["username": $0.username]) }
         print(result)
-        
+
         return Json(result)
     }
-    
+
 }
